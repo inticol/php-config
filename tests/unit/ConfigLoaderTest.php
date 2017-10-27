d8106be578bea43fb45fe663293b5a7b2cb5dab2
Jhon H. Caicedo <jhcaiced@inticol.com>
2017-10-27 15:22:53 -0500

    Added a test skeleton with phpunit

---
+++ tests/unit/ConfigLoaderTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Inticol\Config\Test;
+
+use PHPUnit\Framework\TestCase;
+
+final class ConfigLoaderTest extends TestCase
+{
+    public function testConfigLoader()
+    {
+        $this->assertTrue(1 > 0);
+    }
+}
